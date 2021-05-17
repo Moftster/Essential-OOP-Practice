52f97808612e8872a95f70cb33ece2032d66ab6b
DavidMoftakhar <david@chillaweb.com>
2021-05-17 20:22:48 +0100

    Lesson 15 dependency injection.

--- index.php
+++ index.php
@@ -1,2 +1,49 @@
 <?php
 
+interface Authors
+{
+    public function getName();
+    public function setName($name);
+}
+
+class Article {
+    protected $title;
+    protected $author;
+
+    public function __construct($title, Authors $author)
+    {
+        $this->title = $title;
+        $this->author = $author;
+    }
+
+    public function getTitle()
+    {
+        return $this->title;
+    }
+
+    public function getAuthor()
+    {
+        return $this->author;
+    }
+}
+
+class Author implements Authors {
+    protected $name;
+
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    public function setName($name)
+    {
+        $this->name = $name;
+    }
+}
+
+$author1 = new Author;
+$author1->setName('Joe');
+
+$article1 = new Article('To PHP and Beyond', $author1);
+
+echo $article1->getTitle() . ' by ' . $article1->getAuthor()->getName();
